c80ae160e4b27aa397e1ad4ba7b15d2b11ff010d
[Kristel] <[kkpelegrino@gmail.com]>
2020-12-11 10:35:37 +0800

    with api security

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -6,10 +6,14 @@ use Illuminate\Http\Response;
 
 trait ApiResponser{
     public function successResponse($data, $code = Response :: HTTP_OK){
-        return response()->json(['data'=>$data],$code);
+        return response()->json(['data'=>$data, 'site' => 1],$code);
     }
     
     public function errorResponse($message,$code){
-        return response()->json(['error'=>$message,'code'=>$code],$code);
+        return response()->json(['error'=>$message,'site' => 1,'code'=>$code],$code);
+    }
+
+    public function errorMessage($message,$code){
+        return response($message,$code)->header('Content-Type', 'application/json');
     }
 }
